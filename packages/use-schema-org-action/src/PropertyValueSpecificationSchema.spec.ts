726a7ef070d9f4f220bb7e18e6cb11552307d27f
William Wong <compulim@hotmail.com>
2024-06-29 03:51:49 -0700

    Fix valibot

--- packages/use-schema-org-action/src/PropertyValueSpecificationSchema.spec.ts
+++ packages/use-schema-org-action/src/PropertyValueSpecificationSchema.spec.ts
@@ -23,3 +23,29 @@ describe('parse from empty string', () => {
   test('should not have valueName', () => expect(actual).not.toHaveProperty('valueName'));
   test('should not have valueRequired', () => expect(actual).not.toHaveProperty('valueRequired'));
 });
+
+describe('parse from object', () => {
+  let actual: PropertyValueSpecification;
+
+  beforeEach(() => {
+    actual = parse(PropertyValueSpecificationSchema(), {
+      '@type': 'PropertyValueSpecificationSchema',
+      valueName: 'abc',
+      valueRequired: true
+    });
+  });
+
+  test('should have valueName of "abc"', () => expect(actual).toHaveProperty('valueName', 'abc'));
+  test('should have valueRequired of true', () => expect(actual).toHaveProperty('valueRequired', true));
+});
+
+describe('parse from empty object', () => {
+  let actual: PropertyValueSpecification;
+
+  beforeEach(() => {
+    actual = parse(PropertyValueSpecificationSchema(), {});
+  });
+
+  test('should not have valueName', () => expect(actual).not.toHaveProperty('valueName'));
+  test('should not have valueRequired', () => expect(actual).not.toHaveProperty('valueRequired'));
+});
--- packages/use-schema-org-action/src/PropertyValueSpecificationSchema.ts
+++ packages/use-schema-org-action/src/PropertyValueSpecificationSchema.ts
@@ -1,53 +1,44 @@
-import {
-  boolean,
-  object,
-  optional,
-  parse,
-  string,
-  transform,
-  union,
-  value,
-  type Output,
-  type StringSchema
-} from 'valibot';
+import { boolean, object, optional, parse, pipe, string, transform, union, value, type InferOutput } from 'valibot';
 
 const PropertyValueSpecificationObjectSchema = object({
   '@type': optional(
-    string([value('PropertyValueSpecificationSchema')]) as StringSchema<'PropertyValueSpecificationSchema'>
+    pipe(string(), value('PropertyValueSpecificationSchema', 'Must be "PropertyValueSpecificationSchema"'))
   ),
   valueName: optional(string()),
   valueRequired: optional(boolean())
 });
 
-const PropertyValueSpecificationStringSchema = transform<
-  StringSchema,
-  Output<typeof PropertyValueSpecificationObjectSchema>
->(string(), value => {
-  const spec: Partial<Output<typeof PropertyValueSpecificationObjectSchema>> = {};
+const PropertyValueSpecificationStringSchema = pipe(
+  string(),
+  transform<string, InferOutput<typeof PropertyValueSpecificationObjectSchema>>(value => {
+    const spec: Partial<InferOutput<typeof PropertyValueSpecificationObjectSchema>> = {};
 
-  const specMap = new Map(
-    value.split(/\s+/g).map((token): [string, string] => {
-      // Split must have at least 1 token.
-      const name = token.split('=', 1)[0] as string;
+    const specMap = new Map(
+      parse(string(), value)
+        .split(/\s+/g)
+        .map((token): [string, string] => {
+          // Split must have at least 1 token.
+          const name = token.split('=', 1)[0] as string;
 
-      return [name, token.slice(name.length + 1)];
-    })
-  );
+          return [name, token.slice(name.length + 1)];
+        })
+    );
 
-  if (specMap.has('name')) {
-    spec.valueName = specMap.get('name');
-  }
+    if (specMap.has('name')) {
+      spec.valueName = specMap.get('name');
+    }
 
-  if (specMap.has('required')) {
-    spec.valueRequired = true;
-  }
+    if (specMap.has('required')) {
+      spec.valueRequired = true;
+    }
 
-  return parse(PropertyValueSpecificationObjectSchema, spec);
-});
+    return parse(PropertyValueSpecificationObjectSchema, spec);
+  })
+);
 
 const PropertyValueSpecificationSchema = () =>
   union([PropertyValueSpecificationObjectSchema, PropertyValueSpecificationStringSchema]);
 
 export default PropertyValueSpecificationSchema;
 
-export type PropertyValueSpecification = Output<typeof PropertyValueSpecificationObjectSchema> | string;
+export type PropertyValueSpecification = InferOutput<typeof PropertyValueSpecificationObjectSchema> | string;
