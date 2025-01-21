d5f19ad766402cc8f6578fe15d9a3a65dfa9ee18
William Wong <compulim@hotmail.com>
2025-01-21 00:33:06 -0800

    Expose VariableMap

--- packages/use-schema-org-action/src/index.ts
+++ packages/use-schema-org-action/src/index.ts
@@ -3,6 +3,7 @@ import { type ActionWithActionStatus } from './ActionWithActionStatus.ts';
 import propertyValueSpecificationSchema, {
   type PropertyValueSpecification
 } from './PropertyValueSpecificationSchema.ts';
+import { type VariableMap } from './VariableMap.ts';
 import useSchemaOrgAction from './useSchemaOrgAction2.ts';
 
 export {
@@ -11,5 +12,6 @@ export {
   useSchemaOrgAction,
   type ActionStatusType,
   type ActionWithActionStatus,
-  type PropertyValueSpecification
+  type PropertyValueSpecification,
+  type VariableMap
 };
