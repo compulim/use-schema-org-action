25e8f53c835a8e96c7616935c60abecda9b19ed1
William Wong <compulim@hotmail.com>
2025-02-03 16:36:46 -0800

    Add sample for bug

---
+++ packages/integration-test/bugTemplate.spec.mjs
@@ -0,0 +1,48 @@
+/** @jest-environment @happy-dom/jest-environment */
+
+import { act, renderHook } from '@testing-library/react';
+import { useSchemaOrgAction } from 'use-schema-org-action';
+
+test('should work', async () => {
+  const handler = jest.fn().mockResolvedValue({});
+
+  // WHEN: Rendered.
+  const renderResult = renderHook(() =>
+    useSchemaOrgAction(
+      {
+        '@type': 'VoteAction',
+        'actionOption-input': { valueName: 'action' }
+      },
+      handler,
+      { actionOption: 'upvote' }
+    )
+  );
+
+  // THEN: Should mark the "actionStatus" as "PotentialActionStatus"
+  expect(renderResult.result.current[0]).toEqual({
+    actionOption: 'upvote',
+    actionStatus: 'PotentialActionStatus'
+  });
+
+  // WHEN: Calling setActionState to update.
+  act(() => renderResult.result.current[1](actionState => ({ ...actionState, actionOption: 'downvote' })));
+
+  // THEN: Should have actionState.actionOption of "downvote".
+  expect(renderResult.result.current[0]).toEqual({
+    actionOption: 'downvote',
+    actionStatus: 'PotentialActionStatus'
+  });
+
+  // WHEN: Calling submit().
+  await act(() => renderResult.result.current[2].submit());
+
+  // THEN: Should have called the handler once.
+  expect(handler).toHaveBeenCalledTimes(1);
+  expect(handler).toHaveBeenLastCalledWith({ actionOption: 'downvote' }, expect.any(Map), expect.any(Object));
+
+  // THEN: Should mark the "actionStatus" as "CompletedActionStatus".
+  expect(renderResult.result.current[0]).toEqual({
+    actionOption: 'downvote',
+    actionStatus: 'CompletedActionStatus'
+  });
+});
